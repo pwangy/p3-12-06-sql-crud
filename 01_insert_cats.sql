c2ae3c2a60605ad90c465ed90442ebcf2158676f
pwangy <blipsandclicks@gmail.com>
2024-03-23 20:03:32 -0700

    complete codealong

---
+++ 01_insert_cats.sql
@@ -0,0 +1,4 @@
+INSERT INTO cats (name, age, breed) VALUES ("Lil' Bub", 1, 'American Shorthair');
+INSERT INTO cats (name, age, breed) VALUES ('Fabio', 10, 'Burmese');
+INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO cats (name, age, breed) VALUES ('Miuki', 5, 'LaPerm');
